36e9be3394ba85f813b6668a7abe7604d67e16df
Mainak Das <23ucc569@lnmiit.ac.in>
2025-07-08 03:25:15 +0530

    ai-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@
 import { useMutation } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
+
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const Page = () => {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: ()=> {
@@ -16,7 +21,8 @@ const Page = () => {
 
   return (
     <div className="p-4 max-w-7xl mx-auto bg-black text-white w-full h-screen">
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "Mainak13" })}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
         Invoke Background Job
       </Button>
     </div>
