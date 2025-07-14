3621dd7c9d73e6918f5e097f1a6299c27befbd48
Mainak Das <23ucc569@lnmiit.ac.in>
2025-07-15 03:54:28 +0530

    projects

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -1,17 +1,18 @@
+import { z } from "zod";
+import { generateSlug } from "random-word-slugs";
+
 import { inngest } from "@/inngest/client";
 import prisma from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 
-import { z } from "zod";
-
-export const messagesRouter = createTRPCRouter({
+export const projectsRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
-    const messages = await prisma.message.findMany({
+    const projects = await prisma.project.findMany({
       orderBy: {
         updatedAt: "desc",
       },
     });
-    return messages;
+    return projects;
   }),
   create: baseProcedure
     .input(
@@ -19,25 +20,31 @@ export const messagesRouter = createTRPCRouter({
         value: z.string()
           .min(1, { message: "Value is required" })
           .max(10000, { message: "Value is too long" }),
-        projectId: z.string().min(1, { message: "Project ID is required" }),
       }),
     )
     .mutation(async ({ input }) => {
-        const createdMessage =  await prisma.message.create({
-            data: {
-              projectId: input.projectId,
-              content: input.value,
-              role: "USER",
-              type: "RESULT", 
+        const createdProject = await prisma.project.create({
+          data: {
+            name: generateSlug(2, {
+              format: "kebab",
+            }),
+            messages: {
+              create: {
+                content: input.value,
+                role: "USER",
+                type: "RESULT",
+              },
             },
+          },
         });
+
         await inngest.send({
             name: 'code-agent/run',
             data: {
               value: input.value,
-              projectId: input.projectId,
+              projectId: createdProject.id,
             },
         });
-        return createdMessage;
+        return createdProject;
     }),
 });
